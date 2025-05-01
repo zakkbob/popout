3a42a216c573d88a1eba5f0466e38c5de22f7620
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-05-02 00:04:42 +0100

    vertical physics

--- bookmarketlet.js
+++ bookmarketlet.js
@@ -1,7 +1,7 @@
 var physicsObjects = [];
 
-const FPS = 60;
-const DELTA_MS = Math.floor(1000/FPS);
+const FPS = 200;
+const DELTA_MS = Math.floor(1000 / FPS);
 const GRAVITY = 1000;
 const SCREEN_HEIGHT = visualViewport.height
 
@@ -9,42 +9,42 @@ const SHOW_BOUNDING_BOXES = true;
 
 function addEventListeners() {
     document.querySelectorAll('button:not(.applied), div:not(.applied), a:not(.applied), input:not(.applied), select:not(.applied), textarea:not(.applied), img:not(.applied), svg:not(.applied), [tabindex]:not([tabindex="-1"]):not(.applied)')
-    .forEach(el => {
-        console.log("foind el");
-        el.removeAttribute("href");
-        el.classList.add("applied");
-        if (isVisible(el)) {
-            el.addEventListener('click', (e) => {
-                e.stopImmediatePropagation();
-                let copy = document.createElement("div");
-                let rect = el.getBoundingClientRect();
-                
-                copy.classList.add("applied");
-                copy.top = rect.top;
-                copy.pos = { x: rect.left, y:  rect.top}
-                copy.prevPos = { x: rect.left, y:  rect.top + 5}
-                copy.rect = { width: rect.width, height: rect.height}
+        .forEach(el => {
+            console.log("foind el");
+            el.removeAttribute("href");
+            el.classList.add("applied");
+            if (isVisible(el)) {
+                el.addEventListener('click', (e) => {
+                    e.stopImmediatePropagation();
+                    let copy = document.createElement("div");
+                    let rect = el.getBoundingClientRect();
 
-                copy.style.position = "fixed";
-                copy.style.width = copy.rect.width + "px";
-                copy.style.height = copy.rect.height + "px";
-                copy.style.overflow = "clip";
-                if (SHOW_BOUNDING_BOXES) {
-                    copy.style.borderColor = "red";
-                    copy.style.borderWidth = "1px";
-                    copy.style.borderStyle = "solid";
-                }
-                updateStylePos(copy);
-                copy.style.zIndex = 100000;
-                copy.innerHTML = el.outerHTML;
-                document.body.appendChild(copy);
-                el.remove();
-                console.log(copy);
+                    copy.classList.add("applied");
+                    copy.top = rect.top;
+                    copy.pos = { x: rect.left, y: rect.top }
+                    copy.prevPos = { x: rect.left, y: rect.top + 5 }
+                    copy.rect = { width: rect.width, height: rect.height }
 
-                physicsObjects.push(copy);
-            });
-        }
-    });
+                    copy.style.position = "fixed";
+                    copy.style.width = copy.rect.width + "px";
+                    copy.style.height = copy.rect.height + "px";
+                    copy.style.overflow = "clip";
+                    if (SHOW_BOUNDING_BOXES) {
+                        copy.style.borderColor = "red";
+                        copy.style.borderWidth = "1px";
+                        copy.style.borderStyle = "solid";
+                    }
+                    updateStylePos(copy);
+                    copy.style.zIndex = 100000;
+                    copy.innerHTML = el.outerHTML;
+                    document.body.appendChild(copy);
+                    el.remove();
+                    console.log(copy);
+
+                    physicsObjects.push(copy);
+                });
+            }
+        });
 
 }
 
@@ -62,14 +62,33 @@ function updateStylePos(el) {
 }
 
 function applyVerlet(el) {
-    let tempPos = {x: el.pos.x, y: el.pos.y}
-    el.pos.x = el.pos.x * 2 - el.prevPos.x + 0 * DELTA_MS/1000 * DELTA_MS/1000
-    el.pos.y = Math.max(Math.min(el.pos.y * 2 - el.prevPos.y + GRAVITY * DELTA_MS/1000 * DELTA_MS/1000, SCREEN_HEIGHT-el.rect.height),0)
+    let tempPos = { x: el.pos.x, y: el.pos.y }
+    el.pos.x = el.pos.x * 2 - el.prevPos.x + 0 * DELTA_MS / 1000 * DELTA_MS / 1000
+    el.pos.y = Math.max(Math.min(el.pos.y * 2 - el.prevPos.y + GRAVITY * DELTA_MS / 1000 * DELTA_MS / 1000, SCREEN_HEIGHT - el.rect.height), 0)
     el.prevPos = tempPos
+}
+
+function fixCollisions(el) {
+    physicsObjects.forEach((other) => {
+        if (el == other) return;
+
+        let yOverlap =  other.pos.y - (el.pos.y + el.rect.height)
+        let xOverlap =  other.pos.x - (el.pos.x + el.rect.width)
+
+        if (el.pos.y <= other.pos.y && el.pos.x <= other.pos.x) {
+            console.log("heigher");
+            console.log(yOverlap)
+            if (yOverlap < 0 && xOverlap < 0) {
+                el.pos.y += yOverlap/2;
+                other.pos.y -= yOverlap/2;
+            }
+        }
+    })
 }
 
 function processPhysics() {
     physicsObjects.forEach(applyVerlet);
+    physicsObjects.forEach(fixCollisions);
     physicsObjects.forEach(updateStylePos);
 }
 
